721a75d106a9cc14e0d702783250f86f37cdf788
TanuSharma2511 <53229637+TanuSharma2511@users.noreply.github.com>
2021-10-14 17:54:41 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    secretOrKey: "secret",
-    MONGOURI: "mongodb+srv://resumeUser:Kavita123@cluster0.fcs6h.mongodb.net/resume_backend?retryWrites=true&w=majority"
+    secretOrKey: "",
+    MONGOURI: ""
 }
